934d883233bae84fe97f855c5ee4a37e8d55e73f
matt448 <matthew.mcmillan@gmail.com>
2015-01-11 03:10:20 +0000

    Trap signals for controlled shutdown with backlight fading

--- tft_update.py
+++ tft_update.py
@@ -5,6 +5,8 @@ import ImageDraw
 import ImageFont
 import glob
 import redis
+import signal
+import sys
 
 #import Adafruit_BBIO.ADC as ADC
 import time
@@ -12,6 +14,7 @@ import time
 import Adafruit_ILI9341 as TFT
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
+import Adafruit_BBIO.PWM as PWM
 from time import sleep
 
 # BeagleBone Black configuration.
@@ -36,6 +39,24 @@ disp.clear()
 # Get a PIL Draw object to start drawing on the display buffer.
 draw = disp.draw()
 
+# Now that the display has been blanked out turn on the backlight
+PWM.start("P9_14", 0)
+
+############################
+## Functions
+##
+##
+
+#Signal handler for controlled process shutdown
+def signal_term_handler(signal, frame):
+    print 'got SIGTERM'
+    #Reduce the duty cycle of the backlight PWM pin in steps to fade out
+    duty_cycle = 100
+    while duty_cycle > 0:
+        duty_cycle -= 1
+        PWM.set_duty_cycle("P9_14", duty_cycle)
+        sleep(0.01)
+    sys.exit(0)
 
 # Define a function to create rotated text.  Unfortunately PIL doesn't have good
 # native support for rotated fonts, but this function can be used to make a 
@@ -58,8 +79,18 @@ def draw_rotated_text(image, text, position, angle, font, fill=(255,0,0)):
 #Setup the analog pins
 #ADC.setup()
 
+
+##############################
+##
+## Main Loop
+##
+
 #Loop forever reading the analog pin and displaying the value on the screen
+duty_cycle = 0
 while True:
+    if duty_cycle < 100:
+        duty_cycle += 10
+        PWM.set_duty_cycle("P9_14", duty_cycle)
     #Take time stamp at the begining of the loop
     start_time = time.time()
     #Grab the speed from redis
@@ -81,5 +112,6 @@ while True:
     #Calculate the difference between the two times.
     #This tells us how long it took to draw the TFT screen. 
     ####print 'DIFF: ' + str(end_time - start_time)
+    signal.signal(signal.SIGTERM, signal_term_handler)
 
 
